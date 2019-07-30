77cf38bfa09e7003acf8ea97d7787a9485fdd72b
dsilva525 <dsilva525@gmail.com>
2019-07-30 17:35:34 -0500

    Finished uploading test.pypi

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# sec_edgar
+# sec-edgar
+A Python package for automatically downloading & updating data from the SEC Edgar database
 
-#### Download all zip files
+#### Install
+    pip install sec-edgar
+
+#### Downloads all zip files and unzips them to package folder
     from sec_edgar.financial_statements import financial_statements
     financial_statements.download()
+    
+#### Specify a directory
+    from sec_edgar.financial_statements import financial_statements
+    financial_statements.download(dest_path='/home/username/destination')
+
+- This will automatically keep track of downloaded files and only download new files 
+when run at a later date
--- setup.py
+++ setup.py
@@ -5,14 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sec_edgar",
-    version="0.0.15",
+    version="0.0.2",
     author="David Silva",
-    description="A package to download and extract the financial statements zip files from https://www.sec.gov",
+    author_email="david.silva2841+python@gmail.com",
+    description="A package for automatically downloading & updating data from the SEC Edgar database",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/davidsilva2841/sec_project",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    data_files=[
+        ('', ['LICENSE.txt']),
+        ('', ['README.md']),
+        ('', ['requirements.txt']),
+        ('', ['MANIFEST.in'])
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
